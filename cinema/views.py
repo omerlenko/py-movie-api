4229dd736a88db0fb990cd00cb03db253250307b
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-06-05 19:34:55 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,5 @@
 from rest_framework.decorators import api_view
-from rest_framework.generics import get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 
