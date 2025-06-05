00c2e38f942d2a36bcdb0b49f19ac1b0ba9fc3fa
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-06-05 17:22:21 +0200

    add movie_list view

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from cinema.models import Movie
+from django.http import JsonResponse
+from cinema.serializers import MovieSerializer
 
-# Create your views here.
+def movie_list(request):
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return JsonResponse(serializer.data, status=200, safe=False)
